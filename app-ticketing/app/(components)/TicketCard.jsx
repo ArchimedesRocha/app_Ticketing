ce10b35775554088eb8fff95f2d410c32e633c46
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2024-10-08 16:32:10 -0300

    Hello World

---
+++ app-ticketing/app/(components)/TicketCard.jsx
@@ -0,0 +1,13 @@
+import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
+
+function TicketCard() {
+  return (
+    <div>
+      <DeleteBlock />
+      <PriorityDisplay />
+    </div>
+  );
+}
+
+export default TicketCard;
